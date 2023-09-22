f059f486d6429adf27f512481f95304a37fa9e12
ogorfti <ww.shanks1@gmail.com>
2023-09-22 17:31:31 +0100

    add an additional check for valid input

--- checkmap/my_strtod.c
+++ checkmap/my_strtod.c
@@ -6,17 +6,38 @@
 /*   By: ogorfti <ogorfti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 15:41:33 by ogorfti           #+#    #+#             */
-/*   Updated: 2023/09/20 15:56:30 by ogorfti          ###   ########.fr       */
+/*   Updated: 2023/09/22 17:28:02 by ogorfti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+void	valid_input(char *str)
+{
+	int	dot;
+	int	i;
+
+	i = 0;
+	dot = 0;
+	while (str[i])
+	{
+		if ((!ft_isdigit(str[i]) && str[i] != '.'
+				&& str[i] != '-') || (i == 0 && str[i] == '.'))
+			error_msg("Error: invalid input!\n");
+		if (str[i] == '.')
+			dot++;
+		i++;
+	}
+	if (dot > 1 || (i > 0 && str[i - 1] == '.'))
+		error_msg("Error: invalid input!\n");
+}
 
 int	ft_sign(char **str, double *res)
 {
 	int	sign;
 
 	sign = 1;
+	valid_input(*str);
 	if (**str == '-')
 	{
 		sign = -1;
